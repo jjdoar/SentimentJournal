6ba1b2bfa48168028c6bdf9e921af2b7e0037a24
Miranda Eng <mirghaeng@gmail.com>
2021-01-23 11:42:21 -0800

    Setup postgres container with psycopg2; still needs work

---
+++ backend/src/README.md
@@ -0,0 +1,9 @@
+# README.md
+
+Download Docker on your machine.
+
+The Dockerfile allows us to build the `[mirghaeng/pgsj](https://hub.docker.com/repository/docker/mirghaeng/pgsj)` image using the command: \
+```
+docker-compose up --build -d
+```
+
---
+++ backend/src/db.py
@@ -0,0 +1,30 @@
+import psycopg2
+
+# connect to the db
+con = psycopg2.connect(
+        host = "localhost",
+        database = "sentiment_journal",
+        user = "postgres",
+        password = "postgres")
+
+# cursor
+cur = con.cursor()
+
+cur.execute("INSERT INTO entry (content, user_id) VALUES (%s, %s)", ("Another post!", 1))
+
+# execute query
+cur.execute("SELECT date, time, content FROM entry")
+
+rows = cur.fetchall()
+
+for r in rows:
+    print(f"{r[0]} {r[1]} {r[2]}")
+
+# commit the transaction
+con.commit()
+
+# close the cursor
+cur.close()
+
+# close the connection
+con.close()
