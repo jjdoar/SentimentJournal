aae9a1d0e8e3ede460af3c061a21cdb66cede1e3
Miranda Eng <mirghaeng@gmail.com>
2021-01-26 12:44:31 -0800

    Update

--- backend/tests/unit_tests/test.py
+++ backend/tests/unit_tests/test.py
@@ -5,7 +5,7 @@ import json
 class TestAPI(unittest.TestCase):
     def test_get(self):
         query = {'startDate': '2020-11-17', 'endDate': '2021-11-17', 'userId': 1}
-        response = requests.get(url="http://0.0.0.0:8081/v0/journal_entries", params=query)
+        response = requests.get(url="http://0.0.0.0:8081/v0/journal_entries", json=query)
         self.assertEqual(response.status_code, requests.codes.ok)
         #self.assertEqual(response.json(),
         #        [{"content":"This is an example post.","date":"2021-01-26","score":0,"userId":1},
@@ -13,12 +13,12 @@ class TestAPI(unittest.TestCase):
 
     def test_put(self):
         data = {"content":"I am creating a post.","date":"2021-01-04","userId":5}
-        response = requests.put(url="http://0.0.0.0:8081/v0/journal_entries", params=data)
+        response = requests.put(url="http://0.0.0.0:8081/v0/journal_entries", json=data)
         self.assertEqual(response.status_code, requests.codes.created)
 
     def test_post(self):
         data = {"content":"I am updating my post.","date":"2021-01-05","userId":5}
-        response = requests.put(url="http://0.0.0.0:8081/v0/journal_entries", params=data)
+        response = requests.put(url="http://0.0.0.0:8081/v0/journal_entries", json=data)
         self.assertEqual(response.status_code, requests.codes.no_content)
 
 if __name__ == '__main__':
