e3f9b61615197d6ce323629f015fa78f32e8a90b
Miranda Eng <mirghaeng@gmail.com>
2021-01-26 14:14:45 -0800

    Update

--- backend/tests/unit_tests/README.md
+++ backend/tests/unit_tests/README.md
@@ -7,23 +7,12 @@ Run all test methods
 python -m unittest -v 
 ```
 
-Run individal test methods
-```
-python -m unittest test.TestAPI.test_get -v 
-python -m unittest test.TestAPI.test_put -v 
-python -m unittest test.TestAPI.test_post -v
-```
-
-To measure code coverage, download coverage.py:
+To measure code coverage, download coverage.py 
 ```
 pip install coverage
 ```
-Then run coverage with:
+Then run coverage with 
 ```
-coverage run -m unittest discover
-```
-Or, to have annotated HTML listing run:
-```
+coverage run -m unittest discover 
 coverage report -m
 ```
-and open htmlcov/index.html in browser
--- backend/tests/unit_tests/test.py
+++ backend/tests/unit_tests/test.py
@@ -3,6 +3,8 @@ import requests
 import json
 
 class TestAPI(unittest.TestCase):
+
+    # TEST: simple get
     def test_get(self):
         query = {'startDate': '2020-11-17', 'endDate': '2021-11-17', 'userId': 1}
         response = requests.get(url="http://0.0.0.0:8081/v0/journal_entries", json=query)
@@ -10,14 +12,34 @@ class TestAPI(unittest.TestCase):
         #self.assertEqual(response.json(),
         #        [{"content":"This is an example post.","date":"2021-01-26","score":0,"userId":1},
         #            {"content":"A second post.","date":"2021-01-26","score":0,"userId":1}])
+    
+    # TEST: empty query
+    def test_get_empty(self):
+        query = {}
+        response = requests.get(url="http://0.0.0.0:8081/v0/journal_entries", json=query)
+        self.assertEqual(response.status_code, requests.codes.bad_request)
 
+    # TEST: no userId
+    def test_get_wrong_key(self):
+        query = {'startDate': '2020-11-17', 'endDate': '2021-11-17'}
+        response = requests.get(url="http://0.0.0.0:8081/v0/journal_entries", json=query)
+        self.assertEqual(response.status_code, requests.codes.bad_request)
+
+    # TEST: userId does not exist in db
+    def test_get_id_DNE(self):
+        query = {'startDate': '2020-11-17', 'endDate': '2021-11-17', 'userId': 202}
+        response = requests.get(url="http://0.0.0.0:8081/v0/journal_entries", json=query)
+        self.assertEqual(response.status_code, requests.codes.bad_request)
+
+    # TEST: simple put
     def test_put(self):
-        data = {"content":"I am creating a post.","date":"2021-01-04","userId":5}
+        data = {"content": "I am creating a post.", "date": "2021-01-04", "userId": 202}
         response = requests.put(url="http://0.0.0.0:8081/v0/journal_entries", json=data)
         self.assertEqual(response.status_code, requests.codes.created)
 
+    # TEST: simple post
     def test_post(self):
-        data = {"content":"I am updating my post.","date":"2021-01-05","userId":5}
+        data = {"content":"I am updating my post.","date":"2021-01-05","userId": 202}
         response = requests.post(url="http://0.0.0.0:8081/v0/journal_entries", json=data)
         self.assertEqual(response.status_code, requests.codes.no_content)
 
