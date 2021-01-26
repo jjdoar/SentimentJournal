03618d85f5250d9c643dcb0f5dcfc62cf63028fd
Daniel Escoto <danielescoto@Daniels-MacBook-Pro.local>
2021-01-25 21:57:26 -0800

    router and cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,29 @@
 import "./App.css";
 import EntrySubmitter from "./EntrySubmitter";
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Calendar from "react-calendar";
-import Box from '@material-ui/core/Box';
+import { Button, Box } from "@material-ui/core";
+
 import "react-calendar/dist/Calendar.css";
 
 function App() {
   const [value, onChange] = useState(new Date());
   return (
-    <div className="App">
+    <div
+      style={{
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+        transform: "translate(-50%, -50%)",
+        textAlign: "center",
+      }}
+    >
+      <Link to="/entry">
+        <Button>New</Button>
+      </Link>
       <Calendar onChange={onChange} value={value} />
-      <EntrySubmitter></EntrySubmitter>
+
       <Box>This Is Where The Return Text Will Be</Box>
     </div>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Cal from "./App";
+import Entry from "./EntrySubmitter";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Cal} />
+        <Route exact path="/entry" component={Entry} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
