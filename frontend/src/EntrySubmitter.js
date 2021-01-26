921277412beeffb56d871e36f035ea4c4b2ed8b9
jjdoar <jjdoar@gmail.com>
2021-01-25 16:27:06 -0800

    Send PUT requests to backend
    Not complete. But a start

--- frontend/src/EntrySubmitter.js
+++ frontend/src/EntrySubmitter.js
@@ -1,8 +1,7 @@
-import "./App.css";
 import React, { useState, useEffect } from "react";
 import { Button, TextareaAutosize } from "@material-ui/core";
 
-function Entry() {
+function EntrySubmitter() {
   const monthNames = [
     "January",
     "February",
@@ -35,10 +34,33 @@ function Entry() {
 
   const onHeightChangeEvent = (height) => {
     console.log("height", height);
-  };
+	};
+	
+	function submitEntry(value, date) {
+
+		const userId = "User_name";
+
+		console.log(value);
+		console.log(date);
+		console.log(userId);
+
+		const url = "";
+
+		//var dataString = JSON.stringify(dataObj);
+
+		// Send put request to the backend
+		const requestOptions = {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({'date': date, 'userId': userId, 'content': value})
+		};
+		fetch(url, requestOptions)
+			.then(response => response.json())
+			.then(data => this.setState({postId: data.id}));
+	}
 
   return (
-    <div className="Entry">
+    <div className="EntrySubmitter">
       <h4>Today is {date}</h4>
       <p>{time}</p>
       <div>
@@ -47,13 +69,13 @@ function Entry() {
           cols={60}
           placeholder="Insert Journal entry here"
           onChange={onChangeEvent}
-          onHeightChange={onHeightChangeEvent}
+					onHeightChange={onHeightChangeEvent}
         />
+				<Button color="primary" onClick={() => submitEntry(value, date)}>Submit</Button> <br />
       </div>
-      <Button color="primary">Submit</Button> <br />
-      <span>{value}</span>
+      {/* <span>{value}</span> */}
     </div>
   );
 }
 
-export default Entry;
+export default EntrySubmitter;
