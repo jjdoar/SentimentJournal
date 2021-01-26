ce0fa923b7012fddb8aa1c8ff05da5fb7492df74
Miranda Eng <mirghaeng@gmail.com>
2021-01-26 09:28:40 -0800

    Starter test

--- backend/tests/unit_tests/test.py
+++ backend/tests/unit_tests/test.py
@@ -1,11 +1,13 @@
 import unittest
 import requests
+import json
 
 class TestAPI(unittest.TestCase):
-
-    def test(self):
-        pass
-
+    def test_get(self):
+        query = {'startDate': '2020-11-17', 'endDate': '2021-11-17', 'userId': 1}
+        response = requests.get(url="http://0.0.0.0:8081/v0/journal_entries", params=json.dumps(query))
+        self.assertEqual(response.status_code, 200, response.json())
+        #self.assertEqual(response.json(), {'key': 'value'})
 
 if __name__ == '__main__':
   unittest.main()
