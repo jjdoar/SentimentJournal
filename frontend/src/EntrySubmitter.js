9e5921ce07160e2f8632554ed04a00f745611eb3
jjdoar <jjdoar@gmail.com>
2021-02-17 14:15:31 -0800

    updated Entry viewer

--- frontend/src/EntrySubmitter.js
+++ frontend/src/EntrySubmitter.js
@@ -45,7 +45,7 @@ function EntrySubmitter() {
     // Send PUT request to the backend
     axios({
       method: "PUT",
-      url: "http://54.151.49.150:8081/v0/journal_entries",
+      url: "http://127.0.0.1:8081/v0/journal_entries",
       data: { "date": current.toISOString().substring(0,10), "userId": userId, "content": value },
     }).then((response) => {
       console.log(response);
