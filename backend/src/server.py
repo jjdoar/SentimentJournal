4971628230c2f337cc54c109af9aa60871beda0a
James Garrett <60763234+jamesagarrett@users.noreply.github.com>
2021-02-17 12:20:48 -0800

    Updating PUT

--- backend/src/server.py
+++ backend/src/server.py
@@ -21,7 +21,7 @@ db = "host='0.0.0.0' dbname=%s user=%s password=%s" % (os.getenv('POSTGRES_DB'),
 conn = psycopg2.connect(db)
 cur = conn.cursor()
 
-@app.route("/v0/journal_entries", methods=['GET', 'PUT', 'POST'])
+@app.route("/v0/journal_entries", methods=['GET', 'PUT'])
 def journal_entries():
    
     request_body = request.get_json()
@@ -66,7 +66,7 @@ def journal_entries():
 
         return make_response(jsonify(journal_entries), 200)
 
-    elif request.method == 'PUT':
+    else:
         if not request_body or not request_body.keys() == {"date", "userId", "content"}:
             return make_response(jsonify({
                 "message": "Invalid request",
@@ -83,37 +83,33 @@ def journal_entries():
             sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
             score = round(sentiment.score, 3)
             
-            query = "INSERT INTO entry (user_id, date, content, score) VALUES ({0}, '{1}', '{2}', {3})"
-            cur.execute(query.format(user_id, date, content, score))
-            conn.commit()
+            query = "".join([
+                "SELECT * FROM entry WHERE user_id = ",
+                str(user_id),
+                " AND date = DATE '",
+                date,
+                "';"
+                ])
 
-            return make_response(jsonify({
-                "message": "Created"
-            }), 201)
+            cur.execute(query)
+            entry_tuple = cur.fetchall()
 
-    elif request.method == 'POST':
-        if not request_body or not request_body.keys() == {"date", "userId", "content"}:
-            return make_response(jsonify({
-                "message": "Invalid request",
-                "error": "Invalid request body"
-            }), 400)
-        else:
-            date = request_body["date"]
-            user_id = request_body["userId"]
-            content = request_body["content"]
+            if entry_tuple == None:
+                query = "INSERT INTO entry (user_id, date, content, score) VALUES ({0}, '{1}', '{2}', {3})"
+                cur.execute(query.format(user_id, date, content, score))
+                conn.commit()
 
-            # Detects the sentiment of the text
-            document = language_v1.Document(content=content, type_=language_v1.Document.Type.PLAIN_TEXT)
-            sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
-            score = round(sentiment.score, 3)
+                return make_response(jsonify({
+                    "message": "Created"
+                }), 201)
+            else:
+                query = "UPDATE entry SET content = '{0}', score = {3} WHERE user_id = {1} AND date = '{2}'"
+                cur.execute(query.format(content, score, user_id, date))
+                conn.commit()
 
-            query = "UPDATE entry SET content = '{0}', score = {3} WHERE user_id = {1} AND date = '{2}'"
-            cur.execute(query.format(content, score, user_id, date))
-            conn.commit()
-
-            return make_response(jsonify({
-                "message": "Updated"
-            }), 204)
+                return make_response(jsonify({
+                    "message": "Updated"
+                }), 204)
 
 
 if __name__ == '__main__':
